3fd31c16179eb7b29d7d3cf4121308d772d5378c
tracer-nju <tracer@nju.edu.cn>
2024-04-09 22:32:34 +0800

    Linux lingaoliu 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:32:34 up 1 day, 16:14,  2 users,  load average: 0.07, 0.13, 0.10

---
+++ .shadow/helloprint/hello_print.c
@@ -0,0 +1,21 @@
+#include <am.h>
+#include <stdarg.h>
+
+void print(const char *s, ...)
+{
+    va_list ap;
+    va_start(ap, s);
+    while (s)
+    {
+        for (; *s; s++)
+            putch(*s);
+        s = va_arg(ap, const char *);
+    }
+    va_end(ap);
+}
+
+int main(const char *args)
+{
+    print("\"", args, "\"", " from " __ISA__ " program!\n", NULL);
+    return 0;
+}
