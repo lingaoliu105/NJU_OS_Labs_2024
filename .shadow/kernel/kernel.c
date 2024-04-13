05558a6133cd0fad4255fc529661647a0cfd5198
tracer-nju <tracer@nju.edu.cn>
2024-04-13 14:06:52 +0800

    Linux lingaoliu 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:06:52 up 3 days,  8:18,  2 users,  load average: 0.17, 0.17, 0.21

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -8,56 +8,69 @@
 
 #define SIDE 16
 
-static int w, h;  // Screen size
+static int w, h; // Screen size
 
 #define KEYNAME(key) \
-  [AM_KEY_##key] = #key,
+    [AM_KEY_##key] = #key,
 static const char *key_names[] = {AM_KEYS(KEYNAME)};
 
-static inline void puts(const char *s) {
-    for (; *s; s++) putch(*s);
+static inline void puts(const char *s)
+{
+    for (; *s; s++)
+        putch(*s);
 }
 
-void print_key() {
+void print_key()
+{
     AM_INPUT_KEYBRD_T event = {.keycode = AM_KEY_NONE};
     ioe_read(AM_INPUT_KEYBRD, &event);
-    if (event.keycode != AM_KEY_NONE && event.keydown) {
+    if (event.keycode != AM_KEY_NONE && event.keydown)
+    {
         puts("Key pressed: ");
         puts(key_names[event.keycode]);
         puts("\n");
     }
 }
 
-static void draw_tile(int x, int y, int w, int h, uint32_t color) {
+static void draw_tile(int x, int y, int w, int h, uint32_t color)
+{
     uint32_t pixels[w * h]; // WARNING: large stack-allocated memory
     AM_GPU_FBDRAW_T event = {
-            .x = x, .y = y, .w = w, .h = h, .sync = 1,
-            .pixels = pixels,
+        .x = x,
+        .y = y,
+        .w = w,
+        .h = h,
+        .sync = 1,
+        .pixels = pixels,
     };
-    for (int i = 0; i < w * h; i++) {
+    for (int i = 0; i < w * h; i++)
+    {
         pixels[i] = color;
     }
     ioe_write(AM_GPU_FBDRAW, &event);
 }
 
-void splash() {
+void splash()
+{
     AM_GPU_CONFIG_T info = {0};
     ioe_read(AM_GPU_CONFIG, &info);
     w = info.width;
     h = info.height;
 
-
-    for (int x = 0; x * SIDE <= w; x++) {
-        for (int y = 0; y * SIDE <= h; y++) {
-            if ((x & 1) ^ (y & 1)) {
+    for (int x = 0; x * SIDE <= w; x++)
+    {
+        for (int y = 0; y * SIDE <= h; y++)
+        {
+            if ((x & 1) ^ (y & 1))
+            {
                 draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xff0000);
             }
-
         }
     }
 }
 
-void draw_image() {
+void draw_image()
+{
     AM_GPU_CONFIG_T info = {0};
     ioe_read(AM_GPU_CONFIG, &info);
     w = info.width;
@@ -65,15 +78,21 @@ void draw_image() {
     printf("screen width :  %d \n", w);
     printf("screen height: %d\n", h);
     image_info img_info = parse_png_image(target_img);
+    printf("image width: %d\n", img_info.width);
+    printf("image height: %d\n", img_info.height);
+    printf("The image color type is: %d\n", img_info.color_type);
     uint32_t pixel;
-    for (int x = 0; x * SIDE <= w; x++) {
-        for (int y = 0; y * SIDE <= h; y++) {
-            if (img_info.color_type == 2) {
-                printf("The image is RGB\n");
+    for (int x = 0; x * SIDE <= w; x++)
+    {
+        for (int y = 0; y * SIDE <= h; y++)
+        {
+            if (img_info.color_type == 2)
+            {
                 pixel = (target_img[y * img_info.width + x] << 16) | (target_img[y * img_info.width + x + 1] << 8) |
                         (target_img[y * img_info.width + x + 2]);
-            } else {
-                printf("The image color type is: %d\n",img_info.color_type);
+            }
+            else
+            {
                 pixel = 0xffffff;
             }
             draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, pixel);
@@ -81,19 +100,20 @@ void draw_image() {
     }
 }
 
-
 // Operating system is a C program!
-int main(const char *args) {
+int main(const char *args)
+{
     ioe_init();
 
     puts("mainargs = \"");
-    puts(args);  // make run mainargs=xxx
+    puts(args); // make run mainargs=xxx
     puts("\"\n");
 
-//  splash();
+    //  splash();
     draw_image();
     puts("Press any key to see its key code...\n");
-    while (1) {
+    while (1)
+    {
         print_key();
     }
     return 0;
